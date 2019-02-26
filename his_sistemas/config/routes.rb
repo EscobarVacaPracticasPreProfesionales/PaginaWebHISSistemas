dea2c54e87ad3b1ee1d519f9e90f7780322863cd
Fernando Vaca <fdvaca@espol.edu.ec>
2019-02-26 10:28:51 -0500

    contactus and us on rails

--- his_sistemas/config/routes.rb
+++ his_sistemas/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: redirect('/main_page/index')
   get 'main_page/contactus'
   get 'main_page/news'
   get 'main_page/index'
