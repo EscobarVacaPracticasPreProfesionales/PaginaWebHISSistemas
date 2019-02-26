4e2e6f78d548befccf04caaf00a709789b61c9f4
Fernando Vaca <fdvaca@espol.edu.ec>
2019-02-26 14:04:48 -0500

    new and references

--- his_sistemas/config/routes.rb
+++ his_sistemas/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: redirect('/main_page/index')
   get 'main_page/contactus'
-  get 'main_page/news'
+  get 'main_page/references'
   get 'main_page/index'
   get 'main_page/search'
   get 'main_page/services'
