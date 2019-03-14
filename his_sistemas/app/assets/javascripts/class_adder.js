88ad6819e3b606ecd4ebb124fa5b97b929bb93e3
Fernando Vaca <fdvaca@espol.edu.ec>
2019-03-14 14:00:21 -0500

    articles crud

--- his_sistemas/app/assets/javascripts/class_adder.js
+++ his_sistemas/app/assets/javascripts/class_adder.js
@@ -2,3 +2,8 @@ var elements=document.getElementsByClassName("carousel-inner")
 for (var i = 0; i<elements.length; i++) {
 	elements[i].childNodes[1].className+=" active";
 }
+
+var elements=document.getElementsByClassName("carousel-indicators")
+for (var i = 0; i<elements.length; i++) {
+	elements[i].childNodes[1].className+=" active";
+}
