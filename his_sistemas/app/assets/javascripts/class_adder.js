ce58e937ce724f829f0868f3adcf2e2ff67d7904
Jose Escobar <joanesco@espol.edu.ec>
2019-03-14 11:20:37 -0500

    services gg

--- his_sistemas/app/assets/javascripts/class_adder.js
+++ his_sistemas/app/assets/javascripts/class_adder.js
@@ -1,1 +1,4 @@
-document.getElementsByClassName("carousel-item")[0].className+=" active";
+var elements=document.getElementsByClassName("carousel-inner")
+for (var i = 0; i<elements.length; i++) {
+	elements[i].childNodes[1].className+=" active";
+}
