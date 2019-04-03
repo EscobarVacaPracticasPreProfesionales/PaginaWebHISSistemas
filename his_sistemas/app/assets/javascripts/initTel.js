e6212d1539fc8eca312bbd0911a937d298d3a28d
Jose Escobar <joanesco@espol.edu.ec>
2019-04-02 23:58:06 -0500

    international tel

---
+++ his_sistemas/app/assets/javascripts/initTel.js
@@ -0,0 +1,20 @@
+var input = document.getElementsByClassName("phone");
+var hidden=document.getElementsByClassName("phonehidden");
+var iti1=window.intlTelInput(input[0],{
+	nationalMode: true,
+	initialCountry: "ec"
+});
+var iti2=window.intlTelInput(input[1],{
+	nationalMode: true,
+	initialCountry: "ec"
+});
+
+input[0].addEventListener('focusout',function(){
+	hidden[0].value = (iti1.isValidNumber()) ? iti1.getNumber()+"" : "" ;
+	console.log(hidden[0]);
+});
+
+input[1].addEventListener('focusout',function(){
+	hidden[1].value=(iti2.isValidNumber()) ? iti1.getNumber()+"" : "" ;
+});
+
